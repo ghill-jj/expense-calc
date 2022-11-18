26a115a95cd82cf58dac90cf1a635cead22aa150
ghill <ghilljj@gmail.com>
2022-11-18 12:59:31 +0530

    summitingForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css'
 
 
 const ExpenseForm = ()=>{
+    const [enteredTitle, setEnteredTitle]=useState('')
+    const [enteredAmount, setEnteredAmount]=useState('')
+    const [entedDate, setEnteredDate]=useState('')
+
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount: '',
+    //     entedDate:'',
+    // })
     const titleChangeHandler = (event) => {
-        console.log(event.target.value)
+        setEnteredTitle(event.target.value)
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // })
+
+        // setUserInput((prevState)=>{
+        //     return{
+        //         ...prevState,
+        //         enteredTitle: event.target.value
+        //     }
+        // })
     }
-    return <form>
+    const amountChangeHandler =(event)=>{
+        setEnteredAmount(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // })
+    }
+    const dateChangeHandler =(event)=>{
+        setEnteredDate(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     entedDate: event.target.value,
+        // })
+    }
+    const submitHandler = (event) =>{
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(entedDate),
+        }
+        console.log(expenseData)
+    }
+
+    return <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
@@ -14,11 +60,11 @@ const ExpenseForm = ()=>{
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' min='0.01' step='0.01' />
+                <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
                 </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min='2019-01-01' max='2022-12-31' />
+                <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} />
             </div>
             </div> 
             <div className='new-expense__actions'>
